cf538bd94a47970d8dfa75bfe908a5e27233376b
Tomasz Lekenta <lekenta95@gmail.com>
2018-04-21 23:12:44 +0200

    Rozwój pagination

--- src/main/java/pl/edu/wat/pze/kwejk/services/PaginationService.java
+++ src/main/java/pl/edu/wat/pze/kwejk/services/PaginationService.java
@@ -3,17 +3,51 @@ package pl.edu.wat.pze.kwejk.services;
 import org.springframework.stereotype.Service;
 import pl.edu.wat.pze.kwejk.model.Page;
 
+import java.text.MessageFormat;
 import java.util.LinkedList;
 import java.util.List;
 
 @Service
 public class PaginationService {
 
-    public List<Page> getPagesList(Integer aAcitivePage) {
+    private final int MAX_PAGE = 25;
+    private final int NUMBER_OF_PAGES_TO_DISPLAY = 7;
+
+    public List<Page> getPagesList(Integer aActivePage) {
+        if(aActivePage <= NUMBER_OF_PAGES_TO_DISPLAY / 2)
+            return getFirstPages(aActivePage);
+        else if(aActivePage > MAX_PAGE - NUMBER_OF_PAGES_TO_DISPLAY / 2)
+            return getLastPages(aActivePage);
+        else
+            return getMiddlePages(aActivePage);
+    }
+
+    private List<Page> getFirstPages(Integer aActivePage) {
+        return getPagesFromRange(1, NUMBER_OF_PAGES_TO_DISPLAY, aActivePage);
+    }
+
+    private List<Page> getLastPages(Integer aActivePage) {
+        return getPagesFromRange(MAX_PAGE - NUMBER_OF_PAGES_TO_DISPLAY + 1,
+                MAX_PAGE,
+                aActivePage
+        );
+    }
+
+    private List<Page> getMiddlePages(Integer aActivePage) {
+        return getPagesFromRange(aActivePage - NUMBER_OF_PAGES_TO_DISPLAY / 2,
+                aActivePage + NUMBER_OF_PAGES_TO_DISPLAY / 2,
+                aActivePage
+        );
+    }
+
+    private List<Page> getPagesFromRange(Integer aFrom, Integer aTo, Integer aActivePage){
         List<Page> pPagesList = new LinkedList<>();
 
-        for(int i = 1; i <= 10; i++) {
-            boolean isActive = aAcitivePage == i;
+        if(aActivePage > aTo || aActivePage < aFrom)
+            throw new IllegalStateException(MessageFormat.format("Active page is not in selected range! Range is <{0}, {1}> and active page is {2}", aFrom, aTo, aActivePage));
+        
+        for(int i = aFrom; i <= aTo; i++) {
+            boolean isActive = aActivePage == i;
             pPagesList.add(new Page(i, isActive));
         }
 
