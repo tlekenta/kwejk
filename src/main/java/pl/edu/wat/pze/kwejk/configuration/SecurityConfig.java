1a14feff0a88b7973d9df4ab07a4d49c24e8f2b0
marcin.niedzielski <marcin.niedzielski@student.wat.edu.pl>
2018-05-22 17:11:24 +0200

    Dodanie konfiguracji, i strony logowania

---
+++ src/main/java/pl/edu/wat/pze/kwejk/configuration/SecurityConfig.java
@@ -0,0 +1,29 @@
+package pl.edu.wat.pze.kwejk.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .and()
+                .formLogin().loginPage("/login").failureUrl("/login-error");
+
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser(User.withDefaultPasswordEncoder().username("user").password("password").roles("USER"));
+    }
+}
---
+++ src/main/java/pl/edu/wat/pze/kwejk/controller/AuthController.java
@@ -0,0 +1,16 @@
+package pl.edu.wat.pze.kwejk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+
+    @GetMapping("/login")
+    public String loginView() {
+        return "auth/login";
+    }
+
+}
