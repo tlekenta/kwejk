8f7d5cb28386ae04a4dcbe35525ef30c83f6907e
marcin.niedzielski <marcin.niedzielski@student.wat.edu.pl>
2018-05-22 18:11:06 +0200

    Utworzenie strony logowania

--- src/main/java/pl/edu/wat/pze/kwejk/controller/AuthController.java
+++ src/main/java/pl/edu/wat/pze/kwejk/controller/AuthController.java
@@ -1,16 +1,26 @@
 package pl.edu.wat.pze.kwejk.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.edu.wat.pze.kwejk.model.ModelAttributeEnum;
+import pl.edu.wat.pze.kwejk.model.ViewEnum;
 
 @Controller
 @RequestMapping("/auth")
 public class AuthController {
 
     @GetMapping("/login")
-    public String loginView() {
-        return "auth/login";
+    public String loginView(Model aModel) {
+        aModel.addAttribute(ModelAttributeEnum.ACTIVE_VIEW.toString(), ViewEnum.LOGIN);
+        return "index.html";
+    }
+
+    @GetMapping("/logout")
+    public String logoutView(Model aModel) {
+        aModel.addAttribute(ModelAttributeEnum.ACTIVE_VIEW.toString(), ViewEnum.LOGOUT);
+        return "index.html";
     }
 
 }
