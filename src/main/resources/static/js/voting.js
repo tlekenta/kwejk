b62db79f0e5ec7a7b8ad8198d75148173568de9e
Tomek <Lekenta>
2018-06-14 21:11:28 +0200

    fix głosowania w topkach i w profilu

--- src/main/resources/static/js/voting.js
+++ src/main/resources/static/js/voting.js
@@ -1,7 +1,7 @@
 $(function(){
     $('.vote-button').click(function(){
         var pButtonId = $(this).attr('id');
-        $.post('api/picture/points', {action: pButtonId}, function() {
+        $.post('../api/picture/points', {action: pButtonId}, function() {
             updatePoints(pButtonId)
         });
     });
@@ -9,7 +9,7 @@ $(function(){
 
 function updatePoints(aButtonId) {
     var pPictureId = aButtonId.split('_')[1];
-    $.get('api/picture/points/' + pPictureId, function(response) {
+    $.get('../api/picture/points/' + pPictureId, function(response) {
         $('#picture_' + pPictureId).html(response);
     });
 }
